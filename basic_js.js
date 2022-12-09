37353de5ac3b5b7b2cf15b0916fd9d0f939631d2
Jozsef Horvath <entwicklerjoe@gmail.com>
2022-12-09 17:00:13 +0100

    Index_js HTML

--- basic_js.js
+++ basic_js.js
@@ -1,7 +1,14 @@
 /* JavaScript playground - just test for fun :) */
+
 /* Array and object */
 const nameArray = ["Joe", "Ben", "Sarah", "Jennifer"];
 
 const tierObj = [{ "name": "Benji", "tier": "dog", "color": "black&grey", "age": 5 },
 { "name": "Bogi", "tier": "cat", "color": "yellow&white", "age": 8 }];
 
+/* Push, shift, unshift and delete */
+nameArray[2] = "Joachim";
+
+
+
+document.write(nameArray);
