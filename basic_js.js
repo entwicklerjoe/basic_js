2187e636676891fef1cfa5880cb5aa2793fd1b90
Jozsef Horvath <entwicklerjoe@gmail.com>
2022-12-09 16:45:05 +0100

    Array and object

--- basic_js.js
+++ basic_js.js
@@ -1,2 +1,7 @@
 /* JavaScript playground - just test for fun :) */
+/* Array and object */
+const nameArray = ["Joe", "Ben", "Sarah", "Jennifer"];
+
+const tierObj = [{ "name": "Benji", "tier": "dog", "color": "black&grey", "age": 5 },
+{ "name": "Bogi", "tier": "cat", "color": "yellow&white", "age": 8 }];
 
