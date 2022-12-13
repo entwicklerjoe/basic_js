3e17f922aa37b1acf2fb037e7a4702161c988019
Jozsef Horvath <entwicklerjoe@gmail.com>
2022-12-13 17:41:52 +0100

    Object & Function problems solved

--- basic_js.js
+++ basic_js.js
@@ -2,40 +2,21 @@
 
 /* Array and object */
 const nameArray = ["Joe", "Ben", "Sarah", "Jennifer"];
-const tierObj = [{ "name": "Beni", "tier": "dog", "color": "black&grey", "age": 5 },
-{ "name": "Bo", "tier": "cat", "color": "yellow&white", "age": 8 }];
+const carObj = {type: "Fiat", model: "500", color: "white"};
+document.write(carObj.type, carObj.model, carObj.color);
 
 /* Push, shift, unshift and delete */
-nameArray[1] = "Joachim";
-nameArray[3] = ["Ben", "Thomas", "Noel"];
-nameArray[4][2] = "Flash";
-nameArray.push([1, 2, 3, 4]);
-nameArray.unshift("Kroc");
-nameArray.shift();
-nameArray.pop();
+
 
 /* Reusable function */
 function nFunction (p1, p2) {
     return p1 + p2;
 };
-nFunction(2, "money");
+document.write(nFunction(2, "money"));
 
 /* Global scope & functions */
-const myGlobalScope = "funny";
-function notFunnyFunction() {
-    myLocalScope = 1000;
-};
-function funnyFunction() {
-    let result = "";
-    if (typeof myGlobalScope != "string") {
-        result += "myGlobalScope: " + myGlobalScope + "Not a string";
-    } else if (typeof myGlobalScope != "number") {
-        result += "myGlobalScope: " + myGlobalScope + "Not a number";
-    } else {
-        result == myLocalScope + "x need you practice!"
-    document.write(result);
-    };
-};
+
+
 
 /* Functions without return statement */
 let cash = 450;
