968960a3fbbca4852035dffaba5658b70a0d070a
Jozsef Horvath <entwicklerjoe@gmail.com>
2022-12-23 05:54:06 +0100

    Object freeze

--- es6/es6.js
+++ es6/es6.js
@@ -12,8 +12,15 @@ function checkScope() {
 
 /* Arrow function */
 const greeting = (name) => name + " have a nice day!";
-document.write(greeting("John"));
+// document.write(greeting("John"));
 /* Default parameters */
 const multiply = (num1 = 1, num2 = 1) => num1 * num2;
-document.write(multiply());
-document.write(multiply(2, 5));
+// document.write(multiply());
+// document.write(multiply(2, 5));
+
+/* Object freeze */
+const carObj = { type: "Fiat", model: "500", color: "white" };
+Object.freeze(carObj);
+carObj.type = "Audi";
+// console.log(carObj.type);
+
