c055342015e1ab05108a371f0a806fbe454917fb
Jozsef Horvath <entwicklerjoe@gmail.com>
2023-01-09 14:42:23 +0100

    Destructuring Assignment

--- es6/es6.js
+++ es6/es6.js
@@ -1,3 +1,14 @@
+/* Array and object */
+const nameArray = ["Joe", "Ben", "Sarah", "Jennifer"];
+const carObj = { type: "Fiat", model: "500", color: "white" };
+const tier = {
+    1: { min: 1, avg: 10, max: 20 },
+    2: { min: 21, avg: 30, max: 40 },
+    3: { min: 41, avg: 50, max: 60 },
+    4: { min: 61, avg: 70, max: 80 },
+    5: { min: 81, avg: 90, max: 99 }
+};
+
 /* Let, var & function, block scope */
 function checkScope() {
     let i = 'function scope';
@@ -19,9 +30,8 @@ const multiply = (num1 = 1, num2 = 1) => num1 * num2;
 // document.write(multiply(2, 5));
 
 /* Object freeze */
-const carObj = { type: "Fiat", model: "500", color: "white" };
-Object.freeze(carObj);
-carObj.type = "Audi";
+// Object.freeze(carObj);
+// carObj.type = "Audi";
 // console.log(carObj.type);
 
 /* Rest parameter */
@@ -29,7 +39,10 @@ const howMany = (...args) => "You have passed " + args.length + " arguments.";
 // document.write(howMany(0, 2, 3, "bye", [5, 6, 7], {}));
 
 /* Spread operator */
-const nameArray = ["Joe", "Ben", "Sarah", "Jennifer"];
 let nameArrayCopy = [...nameArray];
 // document.write(nameArrayCopy);
+
+/* Destructuring Assignment */
+const {type, model} = carObj;
+// document.write(type);
 
